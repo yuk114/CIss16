72be8f7f7bd825b1a9b4bf8c372788240a318a84
Kyle Yu <kyleyu@Kyles-MBP-2.wireless.newschool.edu>
2017-04-05 19:47:57 -0400

    javaquiz

---
+++ projects/javaex/javaquiz/main.js
@@ -0,0 +1,32 @@
+var me = 'Kyle';
+  console.log(me)
+
+var favNumber = 7;
+  console.log(7)
+
+var favNumber = '7';
+  console.log(7)
+
+Boolean(10 > 9);
+console.log(true)
+
+var myName = ['K, Y, L, E'];
+  console.log(myName)
+
+var diffDataTypes = [4, 5, 'six', 'seven', 8];
+  console.log(diffDataTypes)
+
+var firstName = 'Kyle';
+var lastName  = 'Yu';
+var fullName  = 'Kyle + Yu';
+  console.log(fullName);
+
+50 + 50;
+  console.log(100)
+
+var str = 'supercalifragilisticexpialidocious';
+var n = str.length;
+// 34
+
+var allStudentsHere = true;
+  console.log(allStudentsHere);
---
+++ projects/javaex/main.js
@@ -0,0 +1,52 @@
+var me = 'Kyle';
+  console.log(me)
+
+var favNumber = 7;
+
+var favNumber = '7';
+
+var studentsMissing = true;
+
+var myName = [K, Y, L, E];
+
+var diffDataTypes = [4, 5, 'six', 'seven', 8];
+
+var firstName = 'Kyle';
+var lastName  = 'Yu';
+varfullName  = Kyle + Yu;
+
+50 + 50;
+
+var str = 'supercalifragilisticexpialidocious';
+var n = str.length;
+// 34
+
+6 * 11;
+
+22 * 3;
+
+33 * 2;
+
+66 * 1;
+
+66 / 1;
+
+6 * (9 + 2);
+
+var group1 = ['Carol', 'Kyle', 'Fish'];
+var group2 = ['Vitoria', 'Liz', 'Jissaura'];
+var group3 = ['Ana Sofia', 'Gaurika', 'Moon'];
+
+var allStudentsHere = false;
+
+var studentsMissing = true;
+
+var allGroups = [group1, group2, group3];
+
+for (var index1 = 0; index < allGroups.length; index1++) {
+  console.log('This group is number ' + (index1 + 1));
+
+for (var index2 = 0; index2 <allGroups[index1].length; index2**) {}
+  console.log( all groups[index1] [index2] );
+  }
+}
